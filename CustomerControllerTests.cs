06bf23e5efe4fd573de8949e3ce5d6381c948d94
James Bedford <jamesbedford@protonmail.com>
2021-05-31 21:32:02 +1000

    CustomerController

---
+++ CustomerControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTestsMe
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFoundObject()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnOkObject()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
